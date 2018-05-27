e716153f9d01bb59724a3a36595c87bf2b4381dc
Elpizo Choi <203277+fuJiin@users.noreply.github.com>
2018-05-26 20:02:25 -0700

    Add install, config scripts

---
+++ configure.sh
@@ -0,0 +1,7 @@
+# Link Spacemacs
+# TODO: Make idempotent
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+ln -s /usr/local/Cellar/emacs-plus/**/Emacs.app /Applications
+
+# Set up iTerm
+# http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
