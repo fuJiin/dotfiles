e873276b2644d1e0c8965412f6c6ad9a3b48aaba
Elpizo Choi <fu7iin@gmail.com>
2019-02-22 17:41:40 -0800

    Update install, config scripts, spacemacs configs

--- configure.sh
+++ configure.sh
@@ -8,3 +8,31 @@ fi
 # http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/.homesick/repos/dotfiles/configs/com.googlecode.iterm2.plist"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Basic bash_profile setup
+if [ ! -f ~/.bash_profile ]; then
+
+    # Colors, aliases
+    touch ~/.bash_profile
+    echo "source ~/.bash/colors" >> ~/.bash_profile
+    echo "source ~/.bash/alias" >> ~/.bash_profile
+
+    # pyenv
+    echo -e "\n# pyenv" >> ~/.bash_profile
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+    echo -e '\nif command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+
+    # NVM
+    echo -e "\n# NVM" >> ~/.bash_profile
+    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+    echo '. "/usr/local/opt/nvm/nvm.sh"' >> ~/.bash_profile
+fi
+
+# Python
+pyenv install 3.6.4
+pip install isort autoflake ipdb
+
+# Javascript
+mkdir -p ~/.nvm
+yarn global add tern
